8765e3a2981ff74ffd3f547e5c6698a2ec6e960a
lahodiukoleksandra <81188068+lahodiukoleksandra@users.noreply.github.com>
2021-07-31 14:21:39 +0300

    Задача на проверку спама

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,8 +1,9 @@
 function checkSpam(str) {
-  let string = str.length
-  let s = str.slice(0, maxlength - 1)
-    if ( string > maxlength)
-      return s + '…'
-    else
-      return str
+  if (str.includes('1xbet') || str.includes('1XbeT')) {
+    return true;
+  }
+  if (str.includes('XXX') || str.includes('xxx')) {
+  return true;
+  }
+    return false;
 }
